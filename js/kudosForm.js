3f35aeebdd08234d0090446f3046de4b83bb5dee
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2013-11-30 21:03:14 +0000

    added kudosForm post requst

---
+++ js/kudosForm.js
@@ -0,0 +1,40 @@
+
+	$("#kudosForm").submit ( function (event) {
+
+		console.log (usersObject) ;
+
+		event.preventDefault();
+
+		// Get some values from elements on the page:
+		var form = $( this ) ;
+		var skills = form.find( "input[name='skills']" ).val();
+		var personName = form.find( "input[name='personName']" ).val();
+
+
+		var receiver_id = 1 ;
+
+		$.each(usersObject, function(key, object) {
+			if ( object["name"] == personName )
+			{
+				receiver_id = object["id"] ;
+			}
+		} );
+
+		console.log ( "receiver_id " +receiver_id ) ;
+
+		var data = { "skills" : skills , "sender" : 2 } ;
+
+		var url = "http://kudos.fwd.wf/person/" + receiver_id ;
+
+		var postAction = $.post ( url , data ) ;
+
+		postAction.done(function( data ) {
+			console.log ( data ) ;
+		});
+
+
+		console.log ( skills + " " + personName ) ;
+
+
+
+	}) ;
